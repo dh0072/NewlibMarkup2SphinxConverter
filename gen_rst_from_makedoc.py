1653de3823724865c414c43f831f1dd666b1aca9
Danxue Huang <36866155+dh0072@users.noreply.github.com>
2018-08-19 00:12:36 -0500

    Update on 8/19

---
+++ gen_rst_from_makedoc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+#
+# RTEMS Tools Project (http://www.rtems.org/)
+# Copyright 2018 Danxue Huang (danxue.huang@gmail.com)
+# All rights reserved.
+#
+# This file is part of the RTEMS Tools package in 'rtems-tools'.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import argparse
+import makedoc2rst
+import os
+import sys
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Convert newlib style markup to rst markup'
+    )
+    parser.add_argument(
+        '-c',
+        '--c_file_path',
+        type=str,
+        help='Path of c source file with newlib style comments',
+    )
+    parser.add_argument(
+        '-r',
+        '--rst_file_path',
+        type=str,
+        help='Path of destination file with rst markup',
+    )
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    return parser
+
+
+def main(c_file, rst_file):
+    if not os.path.exists(c_file):
+        print("C source file {f} does not exist!".format(f=c_file))
+        sys.exit(1)
+    if os.path.exists(rst_file) and not os.access(rst_file, os.W_OK):
+        print("Can not write to file {f}".format(f=rst_file))
+        sys.exit(1)
+    makedoc2rst.makedoc2rst(c_file, rst_file).convert()
+
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    main(args.c_file_path, args.rst_file_path)
