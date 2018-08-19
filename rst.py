1c27243f9cf83473e3f235c8bc406f589f12ccbb
Danxue Huang <36866155+dh0072@users.noreply.github.com>
2018-08-19 00:03:38 -0500

    Update rst.py

--- rst.py
+++ rst.py
@@ -40,7 +40,7 @@ def register_processor_command():
         'TRAD_SYNOPSIS': gen_code_block,
         'TYPEDEF': gen_code_block,
         'DESCRIPTION': gen_custom_directives,
-        'INDEX': gen_custom_directives,
+        'INDEX': gen_nothing,
         'RETURNS': gen_custom_directives,
         'PORTABILITY': gen_custom_directives,
         'NOTES': gen_custom_directives,
@@ -53,8 +53,29 @@ def register_processor_command():
         'START': gen_nothing,
         'END': gen_nothing,
         'SEEALSO': gen_custom_directives,
+        'COMMENT': gen_comment_block,
+        'ORIGIN': gen_custom_directives,
     }
 
+def transform_command(command):
+    if command == 'RETURNS':
+        return 'STATUS CODES'
+    elif command == 'SYNOPSIS':
+        return 'CALLING SEQUENCE'
+    else:
+        return command
+
+def transform_text(command, text):
+    text = text.replace('<<', '*')
+    text = text.replace('>>', '*')
+    text = text.replace('\'', '`')
+    if command == 'SYNOPSIS':
+        text = text.replace('<[', '')
+        text = text.replace(']>', '')
+    else:
+        text = text.replace('<[', '``')
+        text = text.replace(']>', '``')
+    return text
 
 def get_command_processor(command):
     command_processor_dict = register_processor_command()
@@ -66,25 +87,28 @@ def get_command_processor(command):
 
 
 def gen_function_summary(command, text):
-    function_name = extract_function_name(text)
+    function_names = extract_function_names(text)
     summary = extract_summary(text)
 
     title = '.. {f}:\n\n{f} - {s}\n'.format(
-        f=function_name,
+        f=', '.join(function_names),
         s=summary.capitalize()
     )
     dashes = '-' * len(text) + '\n'
-    func_name_index = '.. index:: {name}\n'.format(name=function_name)
+    function_names_index = ''
+    for function_name in function_names:
+        function_names_index += '.. index:: {name}\n'.format(name=function_name)
     summary_index = '.. index:: {summary}\n\n'.format(summary=summary)
-    return title + dashes + func_name_index + summary_index
+    return title + dashes + function_names_index + summary_index
 
 
-def extract_function_name(text):
-    function_name = ''
-    function_name_match = re.match('\s+(<<(>(?!>)|[^>])*>>)', text)
-    if function_name_match:
-        function_name = function_name_match.group(1).lstrip('<<').rstrip('>>')
-    return function_name
+def extract_function_names(text):
+    function_names = []
+    function_name_matches = re.findall('(\*\S+\*)', text)
+    if function_name_matches:
+        for match in function_name_matches:
+            function_names.append(match.lstrip('*').rstrip('*'))
+    return function_names
 
 
 def extract_summary(text):
@@ -96,8 +120,12 @@ def gen_code_block(command, text):
 
 
 def gen_nothing(command, text):
-    return '\n\n'
+    return ''
 
 
 def gen_custom_directives(command, text):
     return '**{c}:**\n\n{t}\n\n'.format(c=command, t=text)
+
+
+def gen_comment_block(command, text):
+    return '.. {c}: {t}\n\n'.format(c=command, t=text)
